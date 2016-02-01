5b78b030cca2853aba34c9244ca8df30decc40ad
Metin Emenullahi <metin@mtndesign.net>
2016-02-01 14:55:48 +0200

    added token changed username

--- js/globals.js
+++ js/globals.js
@@ -1,7 +1,7 @@
 (function () {
   'use strict';
 
-  window.USERNAME = 'mateogianolio'; // github username
+  window.USERNAME = '0xmtn'; // github username
   window.REPO = 'openblog'; // github repository
   window.TOKEN = 'e70eebb2b683a22e6b4403f8fa3423927404068b'; // github app access token
 }());
